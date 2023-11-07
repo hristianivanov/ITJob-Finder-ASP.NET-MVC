221626f19d6048beca58e05f1d50262cb8698e66
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-11-07 09:23:19 +0200

    Remove comments

--- LuxuryCars/LuxuryCars.Web/Program.cs
+++ LuxuryCars/LuxuryCars.Web/Program.cs
@@ -10,7 +10,6 @@ namespace LuxuryCars.Web
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
 			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(connectionString));
@@ -22,7 +21,6 @@ namespace LuxuryCars.Web
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseMigrationsEndPoint();
@@ -30,7 +28,6 @@ namespace LuxuryCars.Web
 			else
 			{
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
