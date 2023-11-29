5e17c35ba47ae70a38e1d054f001c3d60e589be3
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-11-29 09:31:46 +0200

    Clear unnecessary code on program.cs

--- DevHunter/DevHunter.Web/Program.cs
+++ DevHunter/DevHunter.Web/Program.cs
@@ -1,8 +1,9 @@
 namespace DevHunter.Web
 {
-	using DevHunter.Web.Data;
 	using Microsoft.AspNetCore.Identity;
 	using Microsoft.EntityFrameworkCore;
+
+	using Data;
 
 	public class Program
 	{
@@ -10,19 +11,23 @@ namespace DevHunter.Web
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
 			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(connectionString));
+
 			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-			builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+			builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+				{
+					options.SignIn.RequireConfirmedAccount = true;
+				})
 				.AddEntityFrameworkStores<ApplicationDbContext>();
+
 			builder.Services.AddControllersWithViews();
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseMigrationsEndPoint();
@@ -30,7 +35,6 @@ namespace DevHunter.Web
 			else
 			{
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
