b85e8dbda92d93686e4cf12debca723138858aec
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-12-01 11:27:27 +0200

    Create ApplicationUser

--- DevHunter/DevHunter.Data.Models/ApplicationUser.cs
+++ DevHunter/DevHunter.Data.Models/ApplicationUser.cs
@@ -1,6 +1,24 @@
 ﻿namespace DevHunter.Data.Models
 {
-	public class ApplicationUser
+	using System.ComponentModel.DataAnnotations;
+
+	using Microsoft.AspNetCore.Identity;
+
+	using static Common.EntityValidationConstants.User;
+
+	public class ApplicationUser : IdentityUser<Guid>
 	{
+		public ApplicationUser()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		[Required]
+		[MaxLength(FirstNameMaxLength)]
+		public string FirstName { get; set; } = null!;
+
+		[Required]
+		[MaxLength(LastNameMaxLength)]
+		public string LastName { get; set; } = null!;
 	}
 }
