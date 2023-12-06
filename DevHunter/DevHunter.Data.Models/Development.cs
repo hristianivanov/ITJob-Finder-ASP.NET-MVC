0756e172ef9f54bb0ccb6b2f9ca4513aa019f847
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-12-06 08:59:53 +0200

    Create first implementation of the database entity classes

---
+++ DevHunter/DevHunter.Data.Models/Development.cs
@@ -0,0 +1,20 @@
+﻿namespace DevHunter.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Development
+    {
+        public Development()
+        {
+            this.Id = Guid.NewGuid();
+            this.Technologies = new HashSet<Technology>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string ImageUrl { get; set; } = null!;
+
+        public ICollection<Technology> Technologies { get; set; }
+    }
+}
