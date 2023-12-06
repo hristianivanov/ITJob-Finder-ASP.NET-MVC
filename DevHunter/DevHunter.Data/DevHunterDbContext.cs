f0e9f61d2921a9885e325e2c51c5c737dc2b2407
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-12-06 09:02:27 +0200

    Create DbSets

--- DevHunter/DevHunter.Data/DevHunterDbContext.cs
+++ DevHunter/DevHunter.Data/DevHunterDbContext.cs
@@ -1,13 +1,19 @@
 ﻿namespace DevHunter.Data
 {
-    using DevHunter.Data.Models;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 	using Microsoft.EntityFrameworkCore;
 
+    using Models;
+
 	public class DevHunterDbContext : IdentityDbContext<ApplicationUser,IdentityRole<Guid>,Guid>
-	{
-		public DevHunterDbContext(DbContextOptions<DevHunterDbContext> options)
+    {
+        public DbSet<JobOffer> JobOffers { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<Technology> Technologies { get; set; } = null!;
+        public DbSet<Development> Developments { get; set; } = null!;
+
+        public DevHunterDbContext(DbContextOptions<DevHunterDbContext> options)
 			: base(options)
 		{
 		}
