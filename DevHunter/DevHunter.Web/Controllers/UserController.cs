2c6e565253ab16350483901222b3f1028bd04a91
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-12-30 20:17:14 +0200

    Remove identity area

---
+++ DevHunter/DevHunter.Web/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace DevHunter.Web.Controllers
+{
+	using DevHunter.Web.ViewModels.User;
+	using DevHunter.Data.Models;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Identity;
+
+	public class UserController : Controller
+	{
+		private readonly SignInManager<ApplicationUser> signInManager;
+		private readonly UserManager<ApplicationUser> userManager;
+
+		public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+		{
+			this.signInManager = signInManager;
+			this.userManager = userManager;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterFormModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return this.View(model);
+			}
+
+			ApplicationUser user = new ApplicationUser();
+
+			await this.userManager.SetEmailAsync(user, model.Email);
+			await this.userManager.SetUserNameAsync(user, model.Email);
+
+			var result = await this.userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded)
+			{
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+
+				return View(model);
+			}
+
+			await this.signInManager.SignInAsync(user, isPersistent: false);
+
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Login(string? returnUrl = null)
+		{
+			await this.HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+			LoginFormModel model = new LoginFormModel()
+			{
+				ReturnUrl = returnUrl,
+			};
+
+			return this.View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginFormModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return this.View();
+			}
+
+			var result =
+				await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+			if (!result.Succeeded)
+			{
+				return this.View(model);
+			}
+
+			return this.Redirect(model.ReturnUrl ?? "/Home/Index");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			string returnUrl = HttpContext.Request.Form["returnUrl"];
+
+			await signInManager.SignOutAsync();
+
+			if (returnUrl != null)
+			{
+				return LocalRedirect(returnUrl);
+			}
+
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
