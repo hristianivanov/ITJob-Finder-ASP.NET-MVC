cf32cbec0d69f4bc30b93d614a30d6ab4832d177
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-01-02 03:13:48 +0200

    adding files via upload

--- DevHunter/DevHunter.Web/wwwroot/js/site.js
+++ DevHunter/DevHunter.Web/wwwroot/js/site.js
@@ -1,4 +1,63 @@
+<<<<<<< Updated upstream
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+=======
+﻿/*************Homepage-job-categories*************/
+$('.section-collapse').on('click', function () {
+    $(this).siblings('.child-term, .mt-25').toggle(200);
+    $(this).toggleClass('collapsed');
+})
+
+function so_smooth_scroll(target = null, duration = 500, offset = 0) {
+    if (!target) {
+        return;
+    }
+
+    $('html, body').stop().animate({
+        scrollTop: target.offset().top - offset
+    }, 500, 'swing');
+}
+
+/*************Login-register-modal*************/
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const wrapper = document.querySelector('.modal-container');
+const closeBtns = Array.from(document.querySelectorAll('.modal-container .close-form'));
+
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+    closeBtns[0].style.display = 'none';
+    setTimeout(() => {
+        closeBtns[1].style.display = 'block';
+    }, 1000);
+});
+
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+    closeBtns[1].style.display = 'none';
+    setTimeout(() => {
+        closeBtns[0].style.display = 'block';
+    }, 1000);
+});
+
+document.getElementById('login-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('.modal-container').classList.add('open');
+    document.querySelector('.curtain').classList.add('open');
+    document.body.classList.add('login-dialog-open');
+});
+
+closeBtns.forEach(btn => btn.addEventListener('click', () => {
+    document.querySelector('.modal-container').classList.remove('open');
+    document.querySelector('.curtain').classList.remove('open');
+    document.body.classList.remove('login-dialog-open');
+}));
+
+document.querySelector('.curtain').addEventListener('click', function () {
+    document.querySelector('.modal-container').classList.remove('open');
+    document.querySelector('.curtain').classList.remove('open');
+    document.body.classList.remove('login-dialog-open');
+});
+>>>>>>> Stashed changes
