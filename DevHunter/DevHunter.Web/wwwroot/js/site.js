fabbb4b53c887d0110b666e15bc6a9ab36f7a0d0
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-01-23 08:18:08 +0200

    ...

--- DevHunter/DevHunter.Web/wwwroot/js/site.js
+++ DevHunter/DevHunter.Web/wwwroot/js/site.js
@@ -65,3 +65,48 @@ function switchDialog(indexToShow, indexToHide, timeout = 1000) {
     }, timeout);
 }
 
+
+ Validation
+ let loginSubmitBtn = document.getElementById('submit-login-btn')
+
+ loginSubmitBtn.addEventListener('click', (e) => {
+     e.preventDefault();
+
+     let form = e.currentTarget.parentElement;
+
+     let validationComplete = validateRegistrationForm(form);
+
+     if (validationComplete) {
+         form.submit();
+     }
+ })
+
+ // Registration form front end validation
+ function validateRegistrationForm(loginForm) {
+     let emailInput = loginForm.querySelector('input[name="Email"]');
+     let passwordInput = loginForm.querySelector('input[name="Password"]');
+     let hasErrors = false;
+
+     let fields = [emailInput, passwordInput];
+
+     fields.forEach(function (field) {
+
+         if (!field.value) {
+             hasErrors = true;
+         }
+
+         if (field.value && field === emailInput) {
+             if (!isEmail(field.value)) {
+                 hasErrors = true;
+             }
+         }
+     });
+
+     return !hasErrors;
+ }
+
+ // Checks if a string is a valid email.
+ function isEmail(email) {
+     let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+     return regex.test(email);
+ }
