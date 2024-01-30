1918e03be90d57038e2096bea8b1701c42aa6afe
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-01-30 11:06:16 +0200

    Add custom pop up toastr notifications messages

--- DevHunter/DevHunter.Web/wwwroot/js/site.js
+++ DevHunter/DevHunter.Web/wwwroot/js/site.js
@@ -68,46 +68,46 @@ function switchDialog(indexToShow, indexToHide, timeout = 1000) {
 
 
 /* Validation*/
- let loginSubmitBtn = document.getElementById('submit-login-btn')
+ //let loginSubmitBtn = document.getElementById('submit-login-btn')
 
- loginSubmitBtn.addEventListener('click', (e) => {
-     e.preventDefault();
+ //loginSubmitBtn.addEventListener('click', (e) => {
+ //    e.preventDefault();
 
-     let form = e.currentTarget.parentElement;
+ //    let form = e.currentTarget.parentElement;
 
-     let validationComplete = validateRegistrationForm(form);
+ //    let validationComplete = validateRegistrationForm(form);
 
-     if (validationComplete) {
-         form.submit();
-     }
- })
+ //    if (validationComplete) {
+ //        form.submit();
+ //    }
+ //})
 
- // Registration form front end validation
- function validateRegistrationForm(loginForm) {
-     let emailInput = loginForm.querySelector('input[name="Email"]');
-     let passwordInput = loginForm.querySelector('input[name="Password"]');
-     let hasErrors = false;
+ //// Registration form front end validation
+ //function validateRegistrationForm(loginForm) {
+ //    let emailInput = loginForm.querySelector('input[name="Email"]');
+ //    let passwordInput = loginForm.querySelector('input[name="Password"]');
+ //    let hasErrors = false;
 
-     let fields = [emailInput, passwordInput];
+ //    let fields = [emailInput, passwordInput];
 
-     fields.forEach(function (field) {
+ //    fields.forEach(function (field) {
 
-         if (!field.value) {
-             hasErrors = true;
-         }
+ //        if (!field.value) {
+ //            hasErrors = true;
+ //        }
 
-         if (field.value && field === emailInput) {
-             if (!isEmail(field.value)) {
-                 hasErrors = true;
-             }
-         }
-     });
+ //        if (field.value && field === emailInput) {
+ //            if (!isEmail(field.value)) {
+ //                hasErrors = true;
+ //            }
+ //        }
+ //    });
 
-     return !hasErrors;
- }
+ //    return !hasErrors;
+ //}
 
- // Checks if a string is a valid email.
- function isEmail(email) {
-     let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-     return regex.test(email);
- }
+ //// Checks if a string is a valid email.
+ //function isEmail(email) {
+ //    let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+ //    return regex.test(email);
+ //}
