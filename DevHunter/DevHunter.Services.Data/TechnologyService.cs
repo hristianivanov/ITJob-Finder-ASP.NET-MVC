902b4e0cebab4ff943fff716016be6baf19e29a8
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-01-31 09:25:24 +0200

    Adding files via upload

---
+++ DevHunter/DevHunter.Services.Data/TechnologyService.cs
@@ -0,0 +1,40 @@
+﻿using DevHunter.Web.ViewModels.Technology;
+
+namespace DevHunter.Services.Data
+{
+	using DevHunter.Data;
+	using DevHunter.Data.Models;
+	using Interfaces;
+	using Microsoft.EntityFrameworkCore;
+
+	public class TechnologyService : ITechnologyService
+	{
+		private readonly DevHunterDbContext dbContext;
+
+		public TechnologyService(DevHunterDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task<bool> TechnologyExistsByNameAsync(string technologyName)
+		{
+			bool exists = await this.dbContext
+				.Technologies
+				.AnyAsync(t => t.Name.ToLower() == technologyName.ToLower());
+
+			return exists;
+		}
+
+		public async Task AddAsync(TechnologyFormModel formModel)
+		{
+			Technology technology = new Technology()
+			{
+				Name = formModel.Name,
+				ImageUrl = formModel.ImageUrl,
+			};
+
+			await this.dbContext.Technologies.AddAsync(technology);
+			await this.dbContext.SaveChangesAsync();
+		}
+	}
+}
---
+++ DevHunter/DevHunter.Web/Controllers/TechnologyController.cs
@@ -0,0 +1,78 @@
+﻿namespace DevHunter.Web.Controllers
+{
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+
+	using Services.Data.Interfaces;
+
+	using ViewModels.Technology;
+
+	using static Common.NotificationMessagesConstants;
+
+	[Authorize]
+	public class TechnologyController : Controller
+	{
+		private readonly ITechnologyService technologyService;
+
+		//TODO: only admin
+
+		public TechnologyController(ITechnologyService technologyService)
+		{
+			this.technologyService = technologyService;
+		}
+
+		public IActionResult Add()
+		{
+			try
+			{
+				TechnologyFormModel formModel = new TechnologyFormModel();
+
+				return View(formModel);
+			}
+			catch (Exception)
+			{
+				return GeneralError();
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(TechnologyFormModel formModel)
+		{
+			bool technologyExists =
+				await this.technologyService.TechnologyExistsByNameAsync(formModel.Name);
+
+			if (technologyExists)
+			{
+				ModelState.AddModelError(nameof(formModel.Name), "Technology with this name already exists!");
+			}
+
+			//TODO: to check code of the image
+
+			if (!ModelState.IsValid)
+			{
+				return View(formModel);
+			}
+
+			try
+			{
+				await this.technologyService.AddAsync(formModel);
+
+				return RedirectToAction("All", "Technology");
+			}
+			catch (Exception)
+			{
+				ModelState.AddModelError(string.Empty,
+					"Unexpected error occurred while trying to add your new technology!");
+			}
+
+			return View(formModel);
+		}
+
+		private IActionResult GeneralError()
+		{
+			TempData[ErrorMessage] = "Unexpected error occurred!";
+
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
