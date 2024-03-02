11c7cc29cea3640a4090523649c610bcb3620728
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-02 20:06:35 +0200

    Seed company role

---
+++ DevHunter/DevHunter.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace DevHunter.Web.Infrastructure.Extensions
+{
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Identity;
+	using Microsoft.Extensions.DependencyInjection;
+
+	using Data.Models;
+
+	public static class WebApplicationBuilderExtensions
+	{
+		/// <summary>
+		/// This method seeds company role if it does not exist.
+		/// Passed email should be valid email of existing user in the application.
+		/// </summary>
+		/// <param name="app"></param>
+		/// <returns></returns>
+		public static IApplicationBuilder SeedCompany(this IApplicationBuilder app)
+		{
+			using IServiceScope scopedServices = app.ApplicationServices.CreateScope();
+
+			IServiceProvider serviceProvider = scopedServices.ServiceProvider;
+
+			RoleManager<IdentityRole<Guid>> roleManager =
+				serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+			Task.Run(async () =>
+				{
+					if (await roleManager.RoleExistsAsync("Company"))
+					{
+						return;
+					}
+
+					IdentityRole<Guid> role =
+						new IdentityRole<Guid>("Company");
+
+					await roleManager.CreateAsync(role);
+				})
+				.GetAwaiter()
+				.GetResult();
+
+			return app;
+		}
+	}
+}
