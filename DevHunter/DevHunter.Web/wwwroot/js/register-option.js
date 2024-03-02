72c7fa6c7097c539724853ee0c108acaf9d5b2fd
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-02 20:04:43 +0200

    Separate javacipts files

---
+++ DevHunter/DevHunter.Web/wwwroot/js/register-option.js
@@ -0,0 +1,32 @@
+﻿const registerOptionContainer = document.getElementById("register-option-container");
+const radioBtns = registerOptionContainer.querySelectorAll("input[type=radio]");
+const radioBtnsContainers = registerOptionContainer.querySelectorAll(".radio-btn");
+const createBtn = registerOptionContainer.querySelector(".section-footer > .create-btn");
+
+const btnHrefAttribute = createBtn.getAttribute("href");
+
+radioBtnsContainers.forEach((container) =>
+    container.addEventListener("click", () => {
+        radioBtnsToDefaultState();
+
+        createBtn.classList.add("active");
+        container.classList.add("active");
+        container.querySelector("input[type=radio]").checked = true;
+
+        if (container.classList.contains("option-company")) {
+            createBtn.textContent = "Join us as a client";
+            createBtn.setAttribute("href", `${btnHrefAttribute}/RegisterCompany`);
+        } else if (container.classList.contains("option-client")) {
+            createBtn.textContent = "Apply as a freelancer";
+            createBtn.setAttribute("href", `${btnHrefAttribute}/Register`);
+        }
+
+    })
+);
+
+function radioBtnsToDefaultState() {
+    radioBtnsContainers.forEach((container) =>
+        container.classList.remove("active")
+    );
+    radioBtns.forEach((btn) => (btn.checked = false));
+}
