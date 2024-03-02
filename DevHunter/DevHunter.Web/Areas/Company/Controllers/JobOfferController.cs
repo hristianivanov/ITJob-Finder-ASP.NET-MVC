5c82f26865a9e32920a16bd455c8b91d12cb5277
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-02 20:03:31 +0200

    Add company area

---
+++ DevHunter/DevHunter.Web/Areas/Company/Controllers/JobOfferController.cs
@@ -0,0 +1,21 @@
+﻿namespace DevHunter.Web.Areas.Company.Controllers
+{
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+
+	[Authorize]
+	[Area("Company")]
+	public class JobOfferController : Controller
+	{
+        public JobOfferController()
+        {
+            
+        }
+
+		[Route("company/postjob")]
+        public IActionResult Add()
+		{
+			return View();
+		}
+	}
+}
