aac3a78c189550b5e608169c9d48931c14e0e038
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-02 11:52:20 +0200

    Implement register company logic

--- DevHunter/DevHunter.Web/Controllers/UserController.cs
+++ DevHunter/DevHunter.Web/Controllers/UserController.cs
@@ -6,16 +6,80 @@
 
 	using Data.Models;
 	using ViewModels.User;
+	using DevHunter.Services.Data.Interfaces;
 
 	public class UserController : Controller
 	{
 		private readonly SignInManager<ApplicationUser> signInManager;
 		private readonly UserManager<ApplicationUser> userManager;
+		private readonly ICompanyService companyService;
 
-		public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+		public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ICompanyService companyService)
 		{
 			this.signInManager = signInManager;
 			this.userManager = userManager;
+			this.companyService = companyService;
+		}
+
+		public IActionResult RegisterOption()
+		{
+			if (User?.Identity?.IsAuthenticated ?? false)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			return View();
+		}
+
+		public IActionResult RegisterCompany()
+		{
+			if (User?.Identity?.IsAuthenticated ?? false)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> RegisterCompany(CompanyRegisterFormModel model)
+		{
+			bool companyExists = await this.companyService.ExistsByNameAsync(model.Name);
+
+			if (companyExists)
+			{
+				ModelState.AddModelError(nameof(model.Name), "Company with this name already exists!");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			ApplicationUser user = new ApplicationUser();
+
+			await this.userManager.SetEmailAsync(user, model.Email);
+			await this.userManager.SetUserNameAsync(user, model.Email);
+
+			var result = await this.userManager.CreateAsync(user, model.Password);
+
+			await this.userManager.AddToRoleAsync(user, "Company");
+
+			if (!result.Succeeded)
+			{
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+
+				return View(model);
+			}
+
+			await this.companyService.AddAsync(model);
+
+			await this.signInManager.SignInAsync(user, isPersistent: false);
+
+			return RedirectToAction("Index", "Home");
 		}
 
 		[HttpPost]
