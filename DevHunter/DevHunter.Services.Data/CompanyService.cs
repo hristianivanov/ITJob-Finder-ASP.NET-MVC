566b8aa819feb0edbf85782eb68b4855e928fceb
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-02 20:05:13 +0200

    Add company service

---
+++ DevHunter/DevHunter.Services.Data/CompanyService.cs
@@ -0,0 +1,32 @@
+﻿namespace DevHunter.Services.Data
+{
+	using DevHunter.Data;
+	using DevHunter.Data.Models;
+	using Interfaces;
+	using Microsoft.EntityFrameworkCore;
+	using Web.ViewModels.User;
+
+	public class CompanyService : ICompanyService
+	{
+		private readonly DevHunterDbContext context;
+
+		public CompanyService(DevHunterDbContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task AddAsync(CompanyRegisterFormModel model)
+		{
+			var company = new Company()
+			{
+				Name = model.Name,
+			};
+
+			await context.Companies.AddAsync(company);
+			await this.context.SaveChangesAsync();
+		}
+
+		public async Task<bool> ExistsByNameAsync(string name) 
+			=> await this.context.Companies.FirstOrDefaultAsync(c => c.Name == name) != null;
+	}
+}
---
+++ DevHunter/DevHunter.Services.Data/Interfaces/ICompanyService.cs
@@ -0,0 +1,10 @@
+﻿namespace DevHunter.Services.Data.Interfaces
+{
+	using Web.ViewModels.User;
+
+	public interface ICompanyService
+	{
+		Task AddAsync(CompanyRegisterFormModel model);
+		Task<bool> ExistsByNameAsync(string name);
+	}
+}
