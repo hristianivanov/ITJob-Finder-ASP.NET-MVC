66793bd0a46b5e1376633bf601a671314ab1d874
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-02 20:07:37 +0200

    Add company register view model

---
+++ DevHunter/DevHunter.Web.ViewModels/User/CompanyRegisterFormModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DevHunter.Web.ViewModels.User
+{
+	using System.ComponentModel.DataAnnotations;
+
+	using Microsoft.AspNetCore.Http;
+
+	using Infrastructure.Extensions;
+	
+	using static Common.EntityValidationConstants.Company;
+
+	public class CompanyRegisterFormModel
+	{
+		public string Email { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+
+		public string Name { get; set; } = null!;
+
+		public IFormFile Image { get; set; } = null!;
+	}
+}
