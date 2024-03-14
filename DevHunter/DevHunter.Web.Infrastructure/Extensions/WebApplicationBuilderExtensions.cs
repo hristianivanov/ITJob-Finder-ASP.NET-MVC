87e9130a6c977a8a76a547f17d6c2ad7ddfad976
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-14 08:23:09 +0200

    Add reflection method for applying services to the application

--- DevHunter/DevHunter.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
+++ DevHunter/DevHunter.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -7,16 +7,50 @@
 	using Data.Models;
 
 	using static Common.GeneralApplicationConstants;
+    using System.Reflection;
 
 	public static class WebApplicationBuilderExtensions
 	{
-		/// <summary>
-		/// This method seeds company role if it does not exist.
-		/// Passed email should be valid email of existing user in the application.
-		/// </summary>
-		/// <param name="app"></param>
-		/// <returns></returns>
-		public static IApplicationBuilder SeedCompany(this IApplicationBuilder app)
+        /// <summary>
+        /// This method registers all services with their interfaces and implementations of given assembly.
+        /// The assembly is taken from the type of any service implementation provided.
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void AddApplicationServices(this IServiceCollection services, Type serviceType)
+        {
+            Assembly? serviceAssembly = Assembly.GetAssembly(serviceType);
+            if (serviceAssembly == null)
+            {
+                throw new InvalidOperationException("Invalid service type provided!");
+            }
+
+            Type[] serviceTypes = serviceAssembly
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("Service") && !t.IsInterface)
+                .ToArray();
+
+            foreach (Type implementationType in serviceTypes)
+            {
+                Type? interfaceType = implementationType.GetInterface($"I{implementationType.Name}");
+
+                if (interfaceType == null)
+                {
+                    throw new InvalidOperationException($"No interface is provided for the service with name: {implementationType.Name}");
+                }
+
+                services.AddScoped(interfaceType, implementationType);
+            }
+
+        }
+
+        /// <summary>
+        /// This method seeds company role if it does not exist.
+        /// Passed email should be valid email of existing user in the application.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder SeedCompany(this IApplicationBuilder app)
 		{
 			using IServiceScope scopedServices = app.ApplicationServices.CreateScope();
 
