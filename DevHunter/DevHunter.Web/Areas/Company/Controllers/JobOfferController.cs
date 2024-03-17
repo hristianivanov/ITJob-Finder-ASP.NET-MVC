e03779dd29cae86a47fc151dfdc6525857b2adac
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-17 18:09:47 +0200

    InProgress : job offer CRUD operations

--- DevHunter/DevHunter.Web/Areas/Company/Controllers/JobOfferController.cs
+++ DevHunter/DevHunter.Web/Areas/Company/Controllers/JobOfferController.cs
@@ -44,7 +44,7 @@
 		}
 
 		//TODO: XSS on Description HTMLSanitizer
-        [HttpPost]
+		[HttpPost]
 		[Route("company/postjob")]
 		public async Task<IActionResult> Add(JobOfferFormModel model)
 		{
@@ -56,11 +56,11 @@
 			try
 			{
 				string jobOfferId =
-					await jobOfferService.CreateAndReturnIdAsync(model,this.User.GetId()!);
+					await jobOfferService.CreateAndReturnIdAsync(model, this.User.GetId()!);
 
 				TempData[SuccessMessage] = "Job offer was added successfully!";
 
-				return RedirectToAction("Detail", "JobOffer", new { id = jobOfferId });
+				return RedirectToAction("All");
 			}
 			catch (Exception)
 			{
@@ -69,6 +69,77 @@
 
 				return View(model);
 			}
+		}
+
+		[Route("jobpost/edit")]
+		public async Task<IActionResult> Edit(string id)
+		{
+			try
+			{
+				bool technologyExists = await this.jobOfferService.ExistsByIdAsync(id);
+
+				if (!technologyExists)
+				{
+					TempData[ErrorMessage] = "Technology with the provided id does not exist!";
+
+					return RedirectToAction("All");
+				}
+
+				var model = await this.jobOfferService.GetForEditByIdAsync(id);
+
+				model.Technologies = await this.technologyService.AllAsync();
+
+				return View(model);
+			}
+			catch (Exception)
+			{
+				return GeneralError();
+			}
+		}
+
+		[HttpPost]
+		[Route("jobpost/edit")]
+		public async Task<IActionResult> Edit(string id, JobOfferEditFormModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return this.View();
+			}
+
+			return Ok(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete([FromRoute] string id)
+		{
+			try
+			{
+				bool jobOfferExists = await this.jobOfferService.ExistsByIdAsync(id);
+
+				if (!jobOfferExists)
+				{
+					TempData[ErrorMessage] = "Job post with the provided id does not exist!";
+
+					return RedirectToAction("All");
+				}
+
+				await jobOfferService.DeleteByIdAsync(id);
+
+				TempData[WarningMessage] = "The job post successfully was deleted!";
+
+				return RedirectToAction("All");
+			}
+			catch (Exception)
+			{
+				return GeneralError();
+			}
+		}
+
+		private IActionResult GeneralError()
+		{
+			TempData[ErrorMessage] = "Unexpected error occurred!";
+
+			return RedirectToAction("All");
 		}
 	}
 }
