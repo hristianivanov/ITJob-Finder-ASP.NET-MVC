064ed884298cead289b8b2f65e39879ce2438112
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-03-22 00:49:47 +0200

    Remove files from repo

--- templates/Job-Board-DevBg/js/custom.js
+++
@@ -1,5 +0,0 @@
-$('.section-collapse').on('click', function () {
-    $(this).siblings('.child-term, .mt-25').toggle(200);
-    $(this).toggleClass('collapsed');
-})
-
--- templates/dev.bg - template/wp-content/plugins/so-jobs-enhancements/assets/js/application_success_msg.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('wpcf7mailsent', function(event) {
-    $('.devbg-offcanvas-form.open .contact-form-wrap').hide();
-    $('.devbg-offcanvas-form.open').css('display', 'flex');
-    $('.devbg-offcanvas-form.open .thank-you-wrap').removeClass('d-none');
-}, false);
