d8241fe63a6290ecc33fcfa14c669a3d63abefb0
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-04-08 20:55:33 +0300

    Fix bugs

---
+++ DevHunter/DevHunter.Web.ViewModels/User/UserManageFormModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DevHunter.Web.ViewModels.User
+{
+	public class UserManageFormModel
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
