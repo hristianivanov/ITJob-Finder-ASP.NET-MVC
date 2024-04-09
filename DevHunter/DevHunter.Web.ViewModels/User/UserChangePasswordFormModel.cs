f9b44a65063ff6aef249715d41d5590e5717178a
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-04-09 11:07:30 +0300

    Bug fix : change password

---
+++ DevHunter/DevHunter.Web.ViewModels/User/UserChangePasswordFormModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DevHunter.Web.ViewModels.User
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class UserChangePasswordFormModel
+	{
+		[Required(ErrorMessage = "The Password field is required.")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; } = null!;
+
+		[DataType(DataType.Password)]
+		[Required(ErrorMessage = "The Confirm Password field is required.")]
+		[Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; } = null!;
+	}
+}
