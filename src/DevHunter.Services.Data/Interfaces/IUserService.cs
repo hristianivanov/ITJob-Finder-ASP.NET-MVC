d1bd3991b6656a3c694d25d1bde408d0fcd91549
Hristian Ivanow <hristianfancha@gmail.com>
2026-06-28 08:52:23 +0300

    refactor: return UserViewModel from GetUserByIdAsync instead of raw entity

--- src/DevHunter.Services.Data/Interfaces/IUserService.cs
+++ src/DevHunter.Services.Data/Interfaces/IUserService.cs
@@ -1,11 +1,10 @@
 ﻿namespace DevHunter.Services.Data.Interfaces
 {
-    using DevHunter.Data.Models;
     using Web.ViewModels.User;
 
     public interface IUserService
     {
         Task<IEnumerable<UserViewModel>> AllAsync();
-        Task<ApplicationUser> GetUserByIdAsync(string userId);
+        Task<UserViewModel> GetUserByIdAsync(string userId);
     }
 }
